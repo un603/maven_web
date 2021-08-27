ef674b1ebab89ceb021c33ef2f784274f8d8ab0e
246841170 <246841170@qq.com>
2021-08-27 14:41:39 +0800

    master 6th commit

--- src/main/java/com/hit/utils/JDBCUtils.java
+++ src/main/java/com/hit/utils/JDBCUtils.java
@@ -52,8 +52,6 @@ public class JDBCUtils {
         System.out.println("Hello World");
         System.out.println("Hello Git");
         System.out.println("Hello Hub");
-        System.out.println("Hello Hub");
-        System.out.println("Hello Hub");
         Connection connection = conns.get();
         if(connection == null){
             try {
